060198dd2d16d7ef754ead64b85210c1d40bf4a6
OmarSoub <omars.soub@gmail.com>
2024-09-08 19:46:11 +0300

    run template

---
+++ template.py
@@ -0,0 +1,65 @@
+
+import os
+from pathlib import Path
+import logging
+
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+project_name = "EndToEndMLProjectGenderClassification"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/data_transformation.py",
+    f"src/{project_name}/components/data_validation.py",
+    f"src/{project_name}/components/evaluation.py",
+    f"src/{project_name}/components/evaluation.py",
+    f"src/{project_name}/utils/training.py",
+    f"src/{project_name}/utils/common.py.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/pipeline/stage_01_data_ingestion.py.py",
+    f"src/{project_name}/pipeline/stage_02_data_validation.py.py",
+    f"src/{project_name}/pipeline/stage_03_data_transformation.py.py",
+    f"src/{project_name}/pipeline/stage_04_training.py.py",
+    f"src/{project_name}/pipeline/stage_05_evaluation.py.py",
+    f"src/{project_name}/pipeline/prediction.py.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
